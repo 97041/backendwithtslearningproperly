eaab797fb6922b7c251b11fba6f65d58a2de90f6
abir781 <hasan.abir3176@gmail.com>
2026-06-02 08:54:40 +0600

    kddd

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
-import Express from "express";
+import Express, { type Request, type Response } from "express";
 const app = Express();
 const port = 3000
 
+app.use(Express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  
+  res.status(200).json({
+    "message" : "Express Server",
+    "author" : "Next Level"
+  })
+})
+
+app.post('/users',async(req:Request,res:Response)=>{
+   console.log(req.body);
 })
 
 app.listen(port, () => {
