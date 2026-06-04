1d1b99b6398f1877bd52cd0c33dc55e329c2473e
abir781 <hasan.abir3176@gmail.com>
2026-06-04 10:09:13 +0600

    it'sdoneexceptenvironmenbased

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import Express, { type Request, type Response } from "express";
 const app = Express();
 const port = 3000
-import {Pool} from "pg"
+import {Pool, Result} from "pg"
 
 app.use(Express.json())
 
@@ -15,7 +15,7 @@ const initDB = async()=>{
     await pool.query(`CREATE TABLE IF NOT EXISTS users(
       id SERIAL PRIMARY KEY,
       name VARCHAR(20),
-      email VARCHAR(20) NOT NULL,
+      email VARCHAR(20) UNIQUE NOT NULL,
       password VARCHAR(20) NOT NULL,
       is_active BOOLEAN DEFAULT true,
       age INT,
@@ -37,8 +37,158 @@ app.get('/', (req, res) => {
   })
 })
 
+
+app.delete('/user/:id',async(req:Request,res:Response)=>{
+  const {id}=req.params;
+
+  try{
+
+    const results= await pool.query(
+      `Delete from users where id=$1`,
+      [id]
+    );
+      res.status(201).json({
+      success:true,
+      message:"User created successfully",
+      data: {},
+    });
+
+  }
+  catch(error:any){
+
+       res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    })
+
+  }
+})
+
+
+app.put("/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email,password,age,is_active} = req.body;
+
+  // console.log("Id : ", id);
+  // console.log({ name, password, age, is_active });
+
+  try {
+    const result = await pool.query(
+      `
+    UPDATE users 
+    SET 
+    name=COALESCE($1,name),
+    email=COALESCE($2,email),
+    password=COALESCE($3,password),
+    is_active=COALESCE($4,is_active),
+    
+    age=COALESCE($5,age)
+    
+
+    WHERE id=$6 RETURNING *
+    `,
+      [name,email, password, is_active, age,id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+    }
+
+    // console.log(result);
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
 app.post('/users',async(req:Request,res:Response)=>{
-   console.log(req.body);
+   const {name,email,password,age} = req.body;
+
+   try{
+    const result = await pool.query(
+      `Insert INTO users(name,email,password,age) VALUES($1,$2,$3,$4) RETURNING *`,
+      [name,email,password,age],
+    );
+
+    res.status(201).json({
+      success:true,
+      message:"User created successfully",
+      data: result.rows[0],
+    });
+   } catch(error: any){
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    })
+   }
+})
+app.get('/allusers',async(req:Request,res:Response)=>{
+ try{
+  const fetchdata= await pool.query(
+    `select *from users`,
+  );
+
+  res.status(200).json({
+    success: true,
+    message:"users fetched successfully from neondb",
+    data: fetchdata.rows,
+  });
+ }
+ catch(error:any){
+
+     res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+    
+ }
+})
+
+app.get('/singleusers/:id',async(req:Request,res:Response)=>{
+  const {id}=req.params;
+ try{
+  const fetchdata= await pool.query(
+    `select *from users where id=$1`,
+    [id],
+  );
+
+     if (fetchdata.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+        data: {},
+      });
+    }
+
+  res.status(200).json({
+    success: true,
+    message:"single user fetched successfully",
+    data: fetchdata.rows[0],
+  });
+ }
+ catch(error:any){
+
+     res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+    
+ }
 })
 
 app.listen(port, () => {
